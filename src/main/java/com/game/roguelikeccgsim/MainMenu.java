28bf0c51dff76640a5100393ad808136fa74b993
mark wheeler 3 <trip1234567890@outlook.com>
2022-03-09 15:53:53 -0700

    Test Commit

--- src/main/java/com/game/roguelikeccgsim/MainMenu.java
+++ src/main/java/com/game/roguelikeccgsim/MainMenu.java
@@ -1,8 +1,15 @@
 package com.game.roguelikeccgsim;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 public class MainMenu {
+    @FXML
+    Button buttonThing;
 
+    public void happens() {
+        buttonThing.setFont(new Font(40));
+    }
 }
